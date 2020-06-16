8496ece3c816854d903d2cca27186b9355eef26e
liudong <liudong.edward@gmail.com>
2020-06-16 23:09:12 +0800

    add init for VideoH264Buffer

--- Sources/Model/Buffer/VideoH264Buffer.swift
+++ Sources/Model/Buffer/VideoH264Buffer.swift
@@ -17,7 +17,7 @@ public protocol VideoH264Buffer: VideoCompressedBuffer {
 
 extension VideoH264Buffer {
     
-    public var formatDescription: CMFormatDescription? {
+    public var formatDescription: CMVideoFormatDescription? {
         return CMSampleBufferGetFormatDescription(sampleBuffer)
     }
     
@@ -39,6 +39,49 @@ extension VideoH264Buffer {
 }
 
 extension VideoH264Buffer {
+    
+    public init(data: Data, timingInfo: CMSampleTimingInfo, formatDescription: CMVideoFormatDescription) throws {
+        var status: OSStatus = noErr
+        
+        var blockBufferOut: CMBlockBuffer?
+        let length = data.count
+        status = CMBlockBufferCreateWithMemoryBlock(allocator: kCFAllocatorDefault,
+                                                    memoryBlock: nil,
+                                                    blockLength: length,
+                                                    blockAllocator: nil,
+                                                    customBlockSource: nil,
+                                                    offsetToData: 0,
+                                                    dataLength: length,
+                                                    flags: 0,
+                                                    blockBufferOut: &blockBufferOut)
+        guard let blockBuffer = blockBufferOut else { throw NSError(domain: NSOSStatusErrorDomain, code: Int(status)) }
+        
+        var data = data
+        status = CMBlockBufferReplaceDataBytes(with: &data,
+                                               blockBuffer: blockBuffer,
+                                               offsetIntoDestination: 0,
+                                               dataLength: length)
+        guard status == noErr else { throw NSError(domain: NSOSStatusErrorDomain, code: Int(status)) }
+        
+        var sampleBufferOut: CMSampleBuffer?
+        var sampleSizes: [Int] = [length]
+        var timingInfo = timingInfo
+        status = CMSampleBufferCreate(allocator: kCFAllocatorDefault,
+                                      dataBuffer: blockBuffer,
+                                      dataReady: true,
+                                      makeDataReadyCallback: nil,
+                                      refcon: nil,
+                                      formatDescription: formatDescription,
+                                      sampleCount: 1,
+                                      sampleTimingEntryCount: 1,
+                                      sampleTimingArray: &timingInfo,
+                                      sampleSizeEntryCount: 1,
+                                      sampleSizeArray: &sampleSizes,
+                                      sampleBufferOut: &sampleBufferOut)
+        guard let sampleBuffer = sampleBufferOut else { throw NSError(domain: NSOSStatusErrorDomain, code: Int(status)) }
+        
+        self.init(sampleBuffer: sampleBuffer)
+    }
     
     public func dataBytes() throws -> Data {
         guard let dataBuffer = CMSampleBufferGetDataBuffer(sampleBuffer) else { throw NSError(domain: NSOSStatusErrorDomain, code: -1) }
